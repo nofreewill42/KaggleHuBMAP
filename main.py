458a052bf7e9da8018e0401a8fed7381f48088e1
nofreewill42 <jonavan01@gmail.com>
2023-05-26 19:37:20 +0200

    yopp

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ nofreewill@nofreewill:/media/nofreewill/8TB-SSD/Visual/hubmap-hacking-the-human-
             self.tile_polygons = tile_polygons
 
             # Drop everything except dataset type 1
-            
+            self.tile_polygons = self.tile_polygons[self.tile_polygons['dataset'] == 1]
 
         def __len__(self):
             return len(self.tile_polygons)
