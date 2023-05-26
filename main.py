21cba2118be5532d9ac225f8b9a61a302ec5365b
nofreewill42 <jonavan01@gmail.com>
2023-05-26 19:11:37 +0200

    prompt simple comment

--- main.py
+++ main.py
@@ -60,6 +60,8 @@ nofreewill@nofreewill:/media/nofreewill/8TB-SSD/Visual/hubmap-hacking-the-human-
         def __getitem__(self, idx):
             # Read in the image
             img_pil = Image.open(ds_path / 'train' / self.tile_polygons.loc[idx, 'id'] / '.tiff')
+            return img_pil, self.tile_polygons.loc[idx, 'annotations']
 
+    ds = HuBMAPDataset(tile_polygons)
 
-    ds 
+    # loa
