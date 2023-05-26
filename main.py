6add4fa4fa053ceaa31ff82569905b2f417836d7
nofreewill42 <jonavan01@gmail.com>
2023-05-26 18:05:47 +0200

    completion two times ( before == and after)

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 
 
-if __na
+if __name__ == '__main__':
 # Read in ds_path.txt content and make a pathlib.Path object
 ds_path = Path(Path('ds_path.txt').read_text().strip())
