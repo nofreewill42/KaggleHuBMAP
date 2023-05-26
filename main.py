407bfb8a67ea24a503be3c1e9cb5c5da0650fff9
nofreewill42 <jonavan01@gmail.com>
2023-05-26 18:07:07 +0200

    correction and prompt

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-# Read in ds_path.txt content and make a pathlib.Path object
-ds_path = Path(Path('ds_path.txt').read_text().strip())
+    # Read in ds_path.txt content and make a pathlib.Path object
+    ds_path = Path(Path('ds_path.txt').read_text().strip())
+
+    '''
+    nofreewill@nofreewill:/media/nofreewill/8TB-SSD/Visual/hubmap-hacking-the-human-vasculature$ ls
+hubmap-hacking-the-human-vasculature.zip  test           wsi_meta.csv
+polygons.jsonl                            tile_meta.csv
+sample_submission.csv                     train
+nofreewill@nofreewill:/media/nofreewill/8TB-SSD/Visual/hubmap-hacking-the-human-vasculature$
+    '''
+
+    
