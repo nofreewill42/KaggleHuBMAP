ee2fff534cf752bfa95b5eed483a7b2cda53ca29
nofreewill42 <jonavan01@gmail.com>
2023-05-26 18:53:04 +0200

    completion

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ nofreewill@nofreewill:/media/nofreewill/8TB-SSD/Visual/hubmap-hacking-the-human-
 
     # Merge the DataFrames
     df = tile_meta.merge(polygons, on='id')
-    df 
+    df = df.merge(wsi_meta, on='source_wsi')
 
     # Print the first five rows of tile_polygons
     print(tile_polygons.head())
